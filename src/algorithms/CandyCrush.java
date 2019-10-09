1da1d8f5172493f91af93a96d310bb8c421ea83f
Ravikanth Valiveti <vvaliveti@wayfair.com>
2019-10-09 17:30:57 -0400

    Adding some algos

---
+++ src/algorithms/CandyCrush.java
@@ -0,0 +1,57 @@
+package algorithms;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+class CandyCrush {
+  public List<Integer> shortest(List<Integer> nums, Map<Integer, List<Integer>> dp) {
+    int len = nums.size(), repeat = 1;
+    List<List<Integer>> children = new ArrayList<>();
+    List<Integer> result = new ArrayList<>();
+    
+    // Terminating conditions
+    if (len < 3) return nums;
+    if (dp.containsKey(nums.hashCode())) return dp.get(nums.hashCode());
+
+    for (int i =0, j= i+1; j < len; j++) {
+      if (nums.get(i) == nums.get(j)) {
+        repeat++;
+      } else  {
+        if (repeat >= 3) {
+          ArrayList<Integer> child = new ArrayList<>(nums);
+          child.subList(i, j).clear();
+          children.add(child);
+          repeat = 1;
+        }
+        i = j;
+      }
+    }
+
+    if (children.isEmpty()) {
+      dp.put(nums.hashCode(), nums);
+      return nums;
+    };
+
+    // Recur
+    for(List<Integer> child: children) {
+      List<Integer> subResult = shortest(child, dp);
+      result = (result.size() > subResult.size()) ? subResult : result;
+    }
+
+    // Memoize
+    dp.put(nums.hashCode(), result);
+
+    return result;
+  } 
+
+  public static void main(String[] args) {
+    CandyCrush c = new CandyCrush();
+    System.out.println(
+      c.shortest(Arrays.asList(1,2,2,2,3,3,3,2,1), new HashMap<>())
+      );
+  }
+}
---
+++ src/algorithms/NextGreater.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+import java.util.Arrays;
+
+/**
+ * 12 -> 21
+ * 121 -> 211
+ * 123453 -> 
+ * 
+ */
+
+
+class NextGreater {
+  public int findNextGreater(int n) throws NumberFormatException {
+    String number = n + "";
+    char[] numArray = number.toCharArray();
+    int len = number.length();
+
+    for (int i = len-2; i >= 0; i--) {
+      for (int j = len-1; j > i; j--) {
+        if (numArray[j] > numArray[i]) {
+          char c = numArray[i];
+          numArray[i] = numArray[j];
+          numArray[j] = c;
+
+          // sort the rest of the array
+          Arrays.sort(numArray, i+1, len);
+          
+          long result = Long.parseLong(new String(numArray));
+          return result > Integer.MAX_VALUE ? -1 : (int)result;
+        }
+      }
+    }
+
+    return  -1;
+  }
+
+  public static void main(String[] args) {
+    NextGreater n = new NextGreater();
+    System.out.println(n.findNextGreater(12222333));
+  }
+}
